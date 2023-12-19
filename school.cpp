d75a605e99aadfbabd25c9a0deede1cc1ee390fa
Mawunyo Hafoba <mawunyohafoba28@gmail.com>
2023-12-19 18:45:02 +0000

    Generated getters and setters for school.h

--- school.cpp
+++ school.cpp
@@ -3,6 +3,66 @@
 School::School()
 {
 }
+
+std::string School::getN_name() const
+{
+    return n_name;
+}
+
+void School::setN_name(const std::string &newN_name)
+{
+    n_name = newN_name;
+}
+
+QDate School::getDate_of_establishment() const
+{
+    return Date_of_establishment;
+}
+
+void School::setDate_of_establishment(const QDate &newDate_of_establishment)
+{
+    Date_of_establishment = newDate_of_establishment;
+}
+
+long long School::getN_number_of_students() const
+{
+    return n_number_of_students;
+}
+
+void School::setN_number_of_students(long long newN_number_of_students)
+{
+    n_number_of_students = newN_number_of_students;
+}
+
+long long School::getN_number_of_teachers() const
+{
+    return n_number_of_teachers;
+}
+
+void School::setN_number_of_teachers(long long newN_number_of_teachers)
+{
+    n_number_of_teachers = newN_number_of_teachers;
+}
+
+std::vector<Teacher> School::getTeachers() const
+{
+    return teachers;
+}
+
+void School::setTeachers(const std::vector<Teacher> &newTeachers)
+{
+    teachers = newTeachers;
+}
+
+std::vector<School_Class<Student> > School::getClasses() const
+{
+    return classes;
+}
+
+void School::setClasses(const std::vector<School_Class<Student> > &newClasses)
+{
+    classes = newClasses;
+}
 }
 
 
--- school.h
+++ school.h
@@ -17,6 +17,19 @@ public:
     School();
 
 
+    std::string getN_name() const;
+    void setN_name(const std::string &newN_name);
+    QDate getDate_of_establishment() const;
+    void setDate_of_establishment(const QDate &newDate_of_establishment);
+    long long getN_number_of_students() const;
+    void setN_number_of_students(long long newN_number_of_students);
+    long long getN_number_of_teachers() const;
+    void setN_number_of_teachers(long long newN_number_of_teachers);
+    std::vector<Teacher> getTeachers() const;
+    void setTeachers(const std::vector<Teacher> &newTeachers);
+    std::vector<School_Class<Student> > getClasses() const;
+    void setClasses(const std::vector<School_Class<Student> > &newClasses);
+
 private:
     std::string n_name;
     QDate Date_of_establishment;
